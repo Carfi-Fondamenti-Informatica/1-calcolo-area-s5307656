07679e611da63c6d56a3596a3e628536c4ca8e6d
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-03-13 21:09:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+  float a=0,b=0,c=0,triangolo=0,quadrato=0,rettangolo=0,trapezio=0;
 
+cout << "Inserire tre valori reali" <<endl;
 cin >> a >> b >> c;
+     triangolo = ((a*b)/2);
+     quadrato = a*a;
+     rettangolo = a*b;
+     trapezio = ((a+b)*c)/2;
+             cout << triangolo << endl;
+             cout << quadrato << endl;
+             cout << rettangolo << endl;
+             cout << trapezio << endl;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    return 0;
 }
